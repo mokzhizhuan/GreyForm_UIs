ab1a6ae703bd5538cd1756506d441dbd1b24285a
Mok Zhi Zhuan(Neo Wolf) <53340265+mokzhizhuan@users.noreply.github.com>
2024-08-12 15:16:37 +0800

    script changes

--- Greyform-linux/Python_Application/rossetupinstallcheck.sh
+++ Greyform-linux/Python_Application/rossetupinstallcheck.sh
@@ -1,3 +1,4 @@
 #setup and install ros libraries
-sudo apt install ros-${ROS_DISTRO}-ament-cmake
-sudo apt install ros-${ROS_DISTRO}-rosidl-default-generators
+sudo apt install ament-cmake
+sudo apt install python3-rosdep python3-rosinstall-generator python3-vcstool build-essential
+
