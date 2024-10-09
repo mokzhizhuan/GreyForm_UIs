1d03a55d27a55556fadbf8fe0896d85d0d328b8d
Mok Zhi Zhuan(Neo Wolf) <53340265+mokzhizhuan@users.noreply.github.com>
2024-10-09 11:52:04 +0800

    file changes

--- Greyform-linux-ros1/src/Python_Application/build/talker_listener/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/Python_Application/src/talker_listener
-# Build directory: /root/catkin_ws/src/Python_Application/build/talker_listener
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
