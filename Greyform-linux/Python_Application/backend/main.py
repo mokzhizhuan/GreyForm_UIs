03582167230cd8014dbb7c66e251cec837c4abda
Mok Zhi Zhuan(Neo Wolf) <53340265+mokzhizhuan@users.noreply.github.com>
2025-04-11 11:18:27 +0800

    no fast api plot

--- Greyform-linux/Python_Application/backend/main.py
+++ Greyform-linux/Python_Application/backend/main.py
@@ -14,30 +14,6 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-@app.get("/api/start-fastapi")
-def start_fastapi():
-    try:
-        env = os.environ.copy()
-        env["DISPLAY"] = ":0"  # Adjust if needed
-        process = subprocess.Popen(
-            ["python3", "launcher.py"],
-            env=env,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            error_message = stderr.decode("utf-8")
-            print(f"[launcher.py ERROR]: {error_message}")
-            return {
-                "status": "error",
-                "message": f"launcher.py failed: {error_message}",
-            }
-        return {"status": "success", "message": "FastAPI started"}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
-
-
 @app.get("/api/launch_qt")
 async def launch_qt():
     try:
