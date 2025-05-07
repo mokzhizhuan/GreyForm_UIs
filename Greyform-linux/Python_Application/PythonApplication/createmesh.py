c2d9ef4240284dc987fcc9f0c17acb059816582e
Mok Zhi Zhuan(Neo Wolf) <53340265+mokzhizhuan@users.noreply.github.com>
2025-05-07 15:03:00 +0800

    changes

--- Greyform-linux/Python_Application/PythonApplication/createmesh.py
+++ Greyform-linux/Python_Application/PythonApplication/createmesh.py
@@ -10,11 +10,9 @@ import numpy as np
 from stl import mesh
 import PythonApplication.interactiveevent as events
 import PythonApplication.exceldatavtk as vtk_data_excel
-import re
 import PythonApplication.actors as createactorvtk
 import PythonApplication.arraystorage as storingelement
 import PythonApplication.ifcextractfiles as extractor
-import cv2
 
 
 # create the imported stl mesh in vtk frame
