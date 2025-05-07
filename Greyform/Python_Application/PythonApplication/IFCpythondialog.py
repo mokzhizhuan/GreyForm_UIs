628109f10ce9c50b03e0037842c1774b71d8c9c5
MokZhizhuan(NeoWolf) <gamerdark44@outlook.com>
2025-05-07 18:03:13 +0800

    remove comment

--- Greyform/Python_Application/PythonApplication/IFCpythondialog.py
+++ Greyform/Python_Application/PythonApplication/IFCpythondialog.py
@@ -77,7 +77,6 @@ class ProgressBarDialogIFC(QDialog):
         value = self.progress_bar.value()
         if value < 100:
             self.progress_bar.setValue(value + 1)
-            # Update progress again after 100 milliseconds
             QTimer.singleShot(100, self.update_progress)
         else:
             self.timer.stop()  # Stop the timer when progress reaches 100%
