96076bf518ae839476f5b93417baaf68cae927d1
MokZhizhuan(NeoWolf) <gamerdark44@outlook.com>
2025-07-23 17:10:08 +0800

    changes

--- Greyform/Python_Application/PythonApplication/dataanalysis.py
+++ Greyform/Python_Application/PythonApplication/dataanalysis.py
@@ -438,7 +438,7 @@ class data_draft(object):
                 boxup,
                 glass_walls,
             )
-            tmptemp Tmpholder.returnalltmps()
+            tmptemp = Tmpholder.returnalltmps()
         df_tmptemp = pd.DataFrame(tmptemp)
         df_visited = pd.DataFrame(stage2_rows)
         df_combined = pd.concat([df_tmptemp, df_visited], ignore_index=True)
