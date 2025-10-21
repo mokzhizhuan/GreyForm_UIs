831ee529597163bb8431d41f088524980bb0da3a
Mok Zhi Zhuan(Neo Wolf) <53340265+mokzhizhuan@users.noreply.github.com>
2025-10-21 12:12:46 +0800

    part 2

--- newui_Greyform/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/newui_Greyform/src/my_robot_wallinterfaces
-# Build directory: /root/catkin_ws/newui_Greyform/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- newui_Greyform/devel/share/common-lisp/ros/my_robot_wallinterfaces/srv/my_robot_wallinterfaces-srv.asd
+++
@@ -1,9 +0,0 @@
-
-(cl:in-package :asdf)
-
-(defsystem "my_robot_wallinterfaces-srv"
-  :depends-on (:roslisp-msg-protocol :roslisp-utils )
-  :components ((:file "_package")
-    (:file "SetLed" :depends-on ("_package_SetLed"))
-    (:file "_package_SetLed" :depends-on ("_package"))
-  ))
